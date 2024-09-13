917bd8f2ed356776689e02a62e1c4869ecceef78
MatthewRawson <mrawson@redev.co.uk>
2024-09-13 13:59:02 +0100

    Updated to use requests helper

--- faceitapi.py
+++ faceitapi.py
@@ -26,21 +26,15 @@ def ExtractMatchCount(userId):
         return None
     
 def IsGameFound(user_id, match_count, t_score, ct_score):
-    match_url = f"https://www.faceit.com/api/stats/v1/stats/time/users/{user_id}/games/csgo?page=0&size={match_count}"
+    url = f"https://www.faceit.com/api/stats/v1/stats/time/users/{user_id}/games/csgo?page=0&size={match_count}"
 
     search_string_1 = f"\"i18\":\"{t_score}"
     search_string_2 = f"\"i18\":\"{ct_score}"
     
-    response = requests.get(match_url)
-    
-    if response.status_code == 200:
-        data_str = response.text
+    data_str = GetTextFromUrlRequest(url)
 
-        found_1 = search_string_1 in data_str
-        found_2 = search_string_2 in data_str
+    found_1 = search_string_1 in data_str
+    found_2 = search_string_2 in data_str
 
-        return found_1 or found_2
+    return found_1 or found_2
             
-    else:
-        print(f"Failed to retrieve data, status code: {response.status_code}")
-        return None
